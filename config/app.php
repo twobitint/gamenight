d4c726008812fe3eaf8731d7bbf4610804f0ec42
Justin Labak <jtlabak@uw.edu>
2016-08-01 16:34:04 -0700

    Add debugbar

--- config/app.php
+++ config/app.php
@@ -150,6 +150,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Barryvdh\Debugbar\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
